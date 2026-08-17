4de2aeb8f083b013358a215c5122024d630dedc9
bender <kudin.dima@gmail.com>
2026-08-17 12:29:13 +0000

    Update src/pages/HomePage/sections/Hero.tsx

--- src/pages/HomePage/sections/Hero.tsx
+++ src/pages/HomePage/sections/Hero.tsx
@@ -15,7 +15,7 @@ const secondaryButton = {
 };
 const items = [
   {
-    imageSrc: "https://picsum.photos/seed/106921932/1200/800"
+    imageSrc: "https://storage.googleapis.com/webild/users/user_3HmIunhUsPJFEwuG291xChkZ9Bs/uploaded-1786969535144-fuft23fo.jpg"
   },
   {
     imageSrc: "https://picsum.photos/seed/1529517650/1200/800"
